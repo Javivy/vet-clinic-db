e94b23229cf1dbcc59a84c090780fea40aa95939
Javivy <84439052+Javivy@users.noreply.github.com>
2022-12-14 00:34:27 -0400

    Create queries

--- data.sql
+++ data.sql
@@ -2,28 +2,28 @@
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Agumon',
-        to_date('Feb-3-2020', 'mm-dd-yyyy'), 
+        to_date('02-3-2020', 'mm-dd-yyyy'), 
         0, 
         true, 
         10.23);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Gabumon',
-        to_date('Nov-15-2018', 'mm-dd-yyyy'), 
+        to_date('11-15-2018', 'mm-dd-yyyy'), 
         2, 
         true, 
         8);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Pikachu',
-        to_date('Jan-7-2021', 'mm-dd-yyyy'), 
+        to_date('01-7-2021', 'mm-dd-yyyy'), 
         1, 
         false, 
         15.04);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Devimon',
-        to_date('May-12-2017', 'mm-dd-yyyy'), 
+        to_date('05-12-2017', 'mm-dd-yyyy'), 
         5, 
         true, 
         11);
--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name like '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '12-31-2019';
+SELECT name FROM animals WHERE escape_attempts < 3;
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = true;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
