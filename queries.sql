3542f1b97f6eb81d481f9cb1efcc8f47e69c40be
Javivy <84439052+Javivy@users.noreply.github.com>
2022-12-16 00:29:51 -0400

    Create transaction and savepoint, update columns from the table

--- queries.sql
+++ queries.sql
@@ -7,3 +7,20 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+UPDATE animals SET species = 'digimon' WHERE name like '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species = '';
+
+BEGIN TRANSACTION;
+DELETE FROM animals; /* DELETE ALL THE ANIMALS */
+ROLLBACK;
+
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '01-01-2022';
+SAVEPOINT after_delete;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO after_delete;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
