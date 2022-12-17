2862a3cfafc6ca7f6b432b87aac91c68c8a2e6f1
Javivy <84439052+Javivy@users.noreply.github.com>
2022-12-16 22:29:14 -0400

    Insert data into species and owners

--- data.sql
+++ data.sql
@@ -76,3 +76,13 @@ VALUES ('Ditto',
         4, 
         true, 
         22);
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
